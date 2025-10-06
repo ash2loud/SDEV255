3bde190c91ee386eaf79e90b9be17ef1d1e020db
ash <ash2silly@proton.me>
2025-10-06 19:37:06 -0400

    oops open password

--- backend/db.js
+++ backend/db.js
@@ -1,4 +1,4 @@
 const mongoose = require('mongoose')
-mongoose.connect("mongodb+srv://ash_dbadmin_7467:44826173@sdev255.o7hky5o.mongodb.net/?retryWrites=true&w=majority&appName=SDEV255",
+mongoose.connect("mongodb+srv://ash_dbadmin_7467:fuckyou@sdev255.o7hky5o.mongodb.net/?retryWrites=true&w=majority&appName=SDEV255",
   {useNewURLParser: true})
 module.exports = mongoose
