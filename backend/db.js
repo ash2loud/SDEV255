68d7522230cf8490dcd35a98e4774aa070242331
ash <ash2silly@proton.me>
2025-10-07 21:40:59 -0400

    changed backed directory

--- backend/db.js
+++ backend/db.js
@@ -1,3 +1,3 @@
 const mongoose = require('mongoose')
-mongoose.connect("mongodb://localhost:27017/test", { useNewUrlParser: true });
+mongoose.connect("mongodb+srv://ash_dbadmin_7467:44826173@sdev255.o7hky5o.mongodb.net/?retryWrites=true&w=majority&appName=SDEV255", { useNewUrlParser: true });
 module.exports = mongoose
