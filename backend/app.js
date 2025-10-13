a2c835ca07982089b63b8e3c86ba9a45c201b95c
ash <ash2silly@proton.me>
2025-10-13 08:21:30 -0400

    changed cors setup i guess

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,10 @@
 const express = require("express");
-var cors = require('cors');
+const cors = require('cors');
 const app = express();
 const router = express.Router();
 const bodyParser = require('body-parser');
 const Thing = require("./models/things");
-app.use(cors({ origin: 'https://ash2loud.github.io' }));
+app.use(cors());
 app.use(express.json());
 app.use(bodyParser.json());
 
