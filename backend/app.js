61503e66312c6f6427f0259231238810ebbeb9e8
ash <ash2silly@proton.me>
2025-10-14 12:30:36 -0400

    dude im starting to get real sick of this

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,16 @@
 const express = require("express");
 const cors = require('cors');
 const app = express();
+
+import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.use(express.static(path.join(__dirname, "../docs")));
+
 const router = express.Router();
 const bodyParser = require('body-parser');
 
